5750416ae41f572abcb4f53df821766b09819a20
Oakar-Kyaw <oakarkyaw7090@gmail.com>
2024-02-05 15:25:30 +0630

    fix

--- app/routes/apiKeyRoute.js
+++ app/routes/apiKeyRoute.js
@@ -4,6 +4,6 @@ const { checkApiKey } = require("../lib/apiKeyHandler")
 module.exports = (app) => {
     app.route("/api/api-key")
        .post(apiKey.createApiKey)
-       .get(checkApiKey, apiKey.getApiKey)
-       .put(checkApiKey)
+    //    .get(checkApiKey, apiKey.getApiKey)
+    //    .put(checkApiKey)
 }

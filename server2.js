1c85b187eb0247c33362e01545e2a2207e98d6f7
Aung Ye Htet <aung.atg1738@gmail.com>
2024-09-09 13:17:15 +0630

    fix:server port change

--- server2.js
+++ server2.js
@@ -6,7 +6,7 @@ const express = require("express"),
   config = require("./config/db"),
   app = express(),
   server = require("http").Server(app),
-  port = 4000;
+  port = 9000;
 app.use(cors({ origin: "*" }));
 // app.use(express.json());
 // app.use(express.urlencoded({extended:true}));
